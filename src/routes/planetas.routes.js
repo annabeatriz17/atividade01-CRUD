5169563dd5027487a6eb9d77bcb2dcfa47a72b40
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-10-03 17:05:13 -0300

    create: post method

--- src/routes/planetas.routes.js
+++ src/routes/planetas.routes.js
@@ -8,28 +8,46 @@ let planetas = [
     nome: "Dev",
     temperatura: "13.0",
     agua: false, // Indicação de existência de água //
-    atm: [ "JS", "NODE", "VS", "Code" ],
-  }
+    atm: ["JS", "NODE", "VS", "Code"],
+  },
 ];
+
 //Rota para buscar todos os elementos do array planetas
 planetasRoutes.get("/", (req, res) => {
   return res.status(200).send(planetas);
 });
 
-//Rota para criar novo filmeMarcante
+//Rota para criar novo Planeta
 planetasRoutes.post("/", (req, res) => {
-  const { titulo, genero, emCartaz } = req.body;
+  const { nome, temperatura, agua, atm } = req.body;
 
-  const novoFilme = {
+  if (!nome || !temperatura || !agua) {
+    return res.status(400).send({
+      message: "Os campos nome, temperatura e agua são obrigatórios!",
+    });
+  }
+
+  // Validação de existência de água
+  if (agua != "sim" && agua != "não") {
+    return res.status(400).send({
+      message: "Digite 'sim' ou 'não'!",
+    });
+  }
+
+  const novoPlaneta = {
     id: Number(Math.floor(Math.random() * 999999) + 1),
-    titulo,
-    genero,
-    emCartaz,
+    nome,
+    temperatura,
+    agua,
+    atm,
   };
 
-  planetas.push(novoFilme);
+  planetas.push(novoPlaneta);
 
-  return res.status(201).send(planetas);
+  return res.status(201).send({
+    message: "Planeta cadastrado!",
+    novoPlaneta,
+  });
 });
 
 //Rota para buscar um elemento específico do array planetas
